5f9fa887cc2a5fdf93a46e0d799b325c5b936d2f
Hristijan Micevski <119975922+kikomicevski@users.noreply.github.com>
2023-07-13 09:42:08 +0200

    Deleted index.css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './App';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
