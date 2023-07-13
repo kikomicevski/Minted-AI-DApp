a7fde07a4b7dd20a51a213cb2e44a2b28e11d41c
Hristijan Micevski <119975922+kikomicevski@users.noreply.github.com>
2023-07-13 09:39:11 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
